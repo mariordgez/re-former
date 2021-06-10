de40c530f9c1d586477a3f3c28438f24a93f745f
Fondem Junior <fondemjunior@yahoo.com>
2021-06-10 23:40:40 +0100

    Add private method in user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,18 @@ class UsersController < ApplicationController
   def new
   end
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(users_params)
   
     if @user.save
       redirect_to new_user_path
     else
-      render :new
+       render :new
     end
   end
+
+  private 
+
+    def users_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
