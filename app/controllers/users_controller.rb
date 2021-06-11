1c65c6934af11e7e2bbaae4584cd8584fb4413e9
Mario Rodriguez <mariordgez@hotmail.com>
2021-06-11 05:45:16 -0500

    Updated controller with edit and update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(users_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private 
 
     def users_params
