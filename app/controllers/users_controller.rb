68dc02a2816d3582d10278931b7e061795e31259
Mario Rodriguez <mariordgez@hotmail.com>
2021-06-11 11:27:33 -0500

    Fixed linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(users_params)
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
-       render :new
+      render :new
     end
   end
 
@@ -26,9 +27,9 @@ class UsersController < ApplicationController
     end
   end
 
-  private 
+  private
 
-    def users_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
