3dc10218aaa2b761dd8ab459b0db0bef02fb028a
Mario Rodriguez <mariordgez@hotmail.com>
2021-06-11 04:59:42 -0500

    Changed form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def new
   end
   def create
-    @user = User.new(users_params)
-  
+   # @user = User.new(users_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
